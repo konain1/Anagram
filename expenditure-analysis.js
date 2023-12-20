ad98a39e2d3bda16a6e06c65b57f4b54a7460387
Konain1 <kaunainkasab@gmail.com>
2023-12-20 21:15:07 +0530

    testing

--- expenditure-analysis.js
+++ expenditure-analysis.js
@@ -13,8 +13,54 @@
   Output - [{ category: 'Food', totalSpent: 10 }] // Can have multiple categories, only one example is mentioned here
 */
 
+let transactions = [
+  {
+    id:2,
+    timestamp:1656076800001,
+    price:12,
+    category:"Food",
+    itemName:"Pizza"
+  },
+  {
+    id:3,
+    timestamp:1656076800002,
+    price:15,
+    category:"Food",
+    itemName:"Burger"
+  }
+]
+
+let totalTransaction = {
+  category: [],
+  Totalprice : []
+}
 function calculateTotalSpentByCategory(transactions) {
-  return [];
+
+  transactions.map((item,key)=>{
+    totalTransaction.category[key] = item.category;
+    totalTransaction.Totalprice[key] = item.price
+
+  })
+
+  return totalTransaction;
 }
 
-module.exports = calculateTotalSpentByCategory;
+// module.exports = calculateTotalSpentByCategory;
+
+let result = calculateTotalSpentByCategory(transactions)
+
+console.log(typeof result)
+
+// for (let key in result) {
+//   if (result.hasOwnProperty(key)) {
+//     console.log(key, result[key]);
+//   }
+// }
+
+
+
+Object.keys(result).forEach(key => {
+  // key is the property name
+  // myObject[key] is the property value
+  console.log(key, result[key]);
+});
